f47abc41344c0c0ca6f08186afc8015bab21ed7a
bisnis778 <155813906+bisnis778@users.noreply.github.com>
2024-01-06 19:01:33 +0700

    Add files via upload

---
+++ social.js
@@ -0,0 +1,12 @@
+document.write(
+'<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div><a id="close-fixedban" onclick="document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><img alt="close" src="https://1.bp.blogspot.com/-KlqIfiFJtf4/YUZuapc3EQI/AAAAAAAAAHk/eJ6C7ejCU44J4AeNO_9Ka8PSO9PFV38zwCNcBGAsYHQ/s24/cancel.png" title="close button" style="vertical-align:middle;" /></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
+'<scr'+'ipt type="text/javascript">'+
+	'atOptions = {'+
+		'\'key\' : \'ecee9914be319a3e316341b1849ccd16\','+
+		'\'format\' : \'iframe\','+
+		'\'height\' : 250,'+
+		'\'width\' : 300,'+
+		'\'params\' : {}'+
+	'};'+
+	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://toprevenuegate.com/tv1vjpnyt?key=ecee9914be319a3e316341b1849ccd16/invoke.js\"></scr' + 'ipt>');"+
+'</scr'+'ipt>');
