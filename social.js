1440747ead491216f7e46749c8e9c2f42124ccc1
bisnis778 <155813906+bisnis778@users.noreply.github.com>
2024-02-23 20:23:27 +0700

    Update social.js

--- social.js
+++ social.js
@@ -2,11 +2,11 @@ document.write(
 '<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div><a id="close-fixedban" onclick="document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><img alt="close" src="https://1.bp.blogspot.com/-KlqIfiFJtf4/YUZuapc3EQI/AAAAAAAAAHk/eJ6C7ejCU44J4AeNO_9Ka8PSO9PFV38zwCNcBGAsYHQ/s24/cancel.png" title="close button" style="vertical-align:middle;" /></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
 '<scr'+'ipt type="text/javascript">'+
 	'atOptions = {'+
-		'\'key\' : \'ecee9914be319a3e316341b1849ccd16\','+
+		'\'key\' : \'60397c2d1f7abce755777c8ad669402b\','+
 		'\'format\' : \'iframe\','+
 		'\'height\' : 250,'+
 		'\'width\' : 300,'+
 		'\'params\' : {}'+
 	'};'+
-	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://profitablegatecpm.com/ecee9914be319a3e316341b1849ccd16/invoke.js\"></scr' + 'ipt>');"+
+	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://profitablegatecpm.com/60397c2d1f7abce755777c8ad669402b/invoke.js\"></scr' + 'ipt>');"+
 '</scr'+'ipt>');
